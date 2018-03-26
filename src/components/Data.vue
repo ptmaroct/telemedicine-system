56d7815add7cb3578742759f94517001152b7ff8
Anuj Sharma <asharma@datopic.com>
2018-03-26 14:47:01 +0530

    Vue demo components maded and route configured

---
+++ src/components/Data.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Hello Data
+    <router-link to="/">Go back</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'data',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js PWA'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
